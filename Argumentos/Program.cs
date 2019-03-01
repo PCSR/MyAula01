a17cc64c688ccd5dd10714092387834269771363
PCSR <pedrocsr_5@hotmail.com>
2019-03-01 12:39:45 +0000

    Arg length- sumario

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,12 +2,18 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// Imprime no ecra os argumentos 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// O programa comeca aqui
+        /// </summary>
         static void Main(string[] args)
         {
 
-            for (int i = 0; i<args.Length ; i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 Console.WriteLine(args[i]);
             }
